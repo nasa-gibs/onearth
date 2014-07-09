44abada8c6dec9cdb103fe6d8f1975fae6421890
mcechini <matthew.f.cechini@nasa.gov>
2014-07-09 17:57:31 -0400

    Update oe_configure_layer.py
    Adding xlink:type and xlink:role for colormap GC metadata element.

--- src/layer_config/bin/oe_configure_layer.py
+++ src/layer_config/bin/oe_configure_layer.py
@@ -1305,7 +1305,8 @@ for conf in conf_files:
             <ows:Title>$Title</ows:Title>
             $BoundingBox
             <ows:Identifier>$Identifier</ows:Identifier>
-            <ows:Metadata xlink:href="$ColorMap" xlink:title="GIBS Color Map: Data - RGB Mapping"/>
+            <ows:Metadata xlink:type="simple" xlink:role="http://earthdata.nasa.gov/gibs/metadata-type/colormap" 
+                xlink:href="$ColorMap" xlink:title="GIBS Color Map: Data - RGB Mapping"/>
             <Style isDefault="true">
                 <ows:Title>default</ows:Title>
                 <ows:Identifier>default</ows:Identifier>
