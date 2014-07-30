c9c99e6b3ed01df42680eca763f5506f7c936362
Joe Roberts <joe.t.roberts@jpl.nasa.gov>
2014-07-30 12:28:41 -0700

    GIBS-470 Error handling added for bad JPEG tile detection

--- src/mrfgen/mrfgen.py
+++ src/mrfgen/mrfgen.py
@@ -644,12 +644,20 @@ if mrf_compression_type.lower() == 'jpeg' or mrf_compression_type.lower() == 'jp
         # Create the identify command.
         identify_command_list=['identify', tile]
         # Execute identify.
-        identify_process = subprocess.Popen(identify_command_list, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-        identify_process.wait()
-        if 'DirectClass' in identify_process.stdout.readlines()[0]:
+        try:
+            identify_process = subprocess.Popen(identify_command_list, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+            identify_process.wait()
+            if 'DirectClass' in identify_process.stdout.readlines()[0]:
+                goodtiles.append(tile)
+            else:
+                try:
+                    sigevent('ERROR', 'Bad JPEG tile detected: ' + tile, sigevent_url)
+                except urllib2.URLError:
+                    print 'sigevent service is unavailable'
+        except OSError:
+            if i==0:
+                log_sig_warn('identify command not found, unable to detect bad JPEG tiles', sigevent_url)
             goodtiles.append(tile)
-        else:
-            sigevent('ERROR', 'Bad JPEG tile detected: ' + tile, sigevent_url)
     alltiles = goodtiles
     
 # Convert TIFF files
