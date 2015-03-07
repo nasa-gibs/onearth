502a815dc5ff5cdf4499de08adf76b1f4ccacf81
Joe Roberts <joe.t.roberts@jpl.nasa.gov>
2015-03-06 16:20:16 -0800

    Removed extra text at the top of mrfgen script

--- src/mrfgen/mrfgen.py
+++ src/mrfgen/mrfgen.py
@@ -6,7 +6,7 @@
 # Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 #   1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
 #   2. Redistributions in binary form must reproduce the above copyright notice,
-#      this list of conditions and the following disclaimer in the documentation and/or!= os.path.dirname(input_dir) other materials provided with the distribution.
+#      this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
 #   3. Neither the name of the California Institute of Technology (Caltech), its operating division the Jet Propulsion Laboratory (JPL),
 #      the National Aeronautics and Space Administration (NASA), nor the names of its contributors may be used to
 #      endorse or promote products derived from this software without specific prior written permission.
