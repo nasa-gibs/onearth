408f2a4c7b57115c1423fc8eea43857fc292d5cd
Joe Roberts <joe.t.roberts@jpl.nasa.gov>
2015-06-17 16:20:32 -0700

    GIBS-944 Fixed REST redirect to prevent request repeats

--- src/mod_onearth/mod_wms.c
+++ src/mod_onearth/mod_wms.c
@@ -1899,8 +1899,8 @@ int rewrite_rest_uri(request_rec *r) {
 		r->args = apr_psprintf(r->pool,"wmts.cgi?SERVICE=%s&REQUEST=%s&VERSION=%s&LAYER=%s&STYLE=%s&TILEMATRIXSET=%s&TILEMATRIX=%s&TILEROW=%s&TILECOL=%s&FORMAT=image%%2F%s","WMTS","GetTile","1.0.0",params[1+d],params[2+d],params[3+d],params[4+d],params[5+d],params[6+d],ap_strcasecmp_match(params[7+d],"jpg") == 0 ? "jpeg" : params[7+d]);
 	if (length == 8)
 		r->args = apr_psprintf(r->pool,"wmts.cgi?SERVICE=%s&REQUEST=%s&VERSION=%s&LAYER=%s&STYLE=%s&TILEMATRIXSET=%s&TILEMATRIX=%s&TILEROW=%s&TILECOL=%s&FORMAT=image%%2F%s&TIME=%s","WMTS","GetTile","1.0.0",params[1+d],params[2+d],params[4+d],params[5+d],params[6+d],params[7+d],ap_strcasecmp_match(params[8+d],"jpg") == 0 ? "jpeg" : params[8+d],params[3+d]);
-	ap_internal_redirect(apr_psprintf(r->pool,"%s/%s",r->uri,r->args),r);
 	ap_log_error(APLOG_MARK,APLOG_WARNING,0,r->server,"REST redirect -> %s/%s",r->uri,r->args);
+	ap_internal_redirect(apr_psprintf(r->pool,"%s/%s",r->uri,r->args),r);
 	return 0;
 }
 
@@ -1912,7 +1912,7 @@ static int handler(request_rec *r) {
 		  if (rewrite_rest_uri(r) < 0)
 			  return DECLINED;
 		  else
-			  return mrf_handler(r);
+			  return OK;
 	  }
 	  else
 		  return DECLINED;
