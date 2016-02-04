8a54391355d9120a01212af44c61f7678c341fad
Joe Roberts <joe.t.roberts@jpl.nasa.gov>
2016-02-03 16:03:19 -0800

    Additional mrfgen test cases

--- src/mrfgen/test_mrfgen.py
+++ src/mrfgen/test_mrfgen.py
@@ -404,7 +404,6 @@ class TestMRFGeneration_OBPG(unittest.TestCase):
         '''
         This portion the following test cases:
             Test using empty MRF with No Copy option
-            Test composite MRF with No Copy option
         '''
         # Check MRF generation succeeded
         self.assertTrue(os.path.isfile(self.output_mrf), "MRF generation failed")
@@ -454,6 +453,7 @@ class TestMRFGeneration_OBPG(unittest.TestCase):
         
         '''
         This portion covers the following test cases:
+            Test composite MRF with No Copy option
             Test using granule images
             Test using existing MRF
             Test using granule images with Z-level
@@ -497,14 +497,9 @@ class TestMRFGeneration_OBPG(unittest.TestCase):
         '''
         This portion covers the following test cases:        
             Test adding image to new Z-level
+            Test adding image to multiple Z-levels
             Test using single image with Z-level
-        
-        Test using tiled images with Z-level
-        Test adding image to multiple Z-levels
-        Test using existing MRF with reprojection
-        Test using existing MRF with reprojection in z-level
         '''
-        
         # Read MRF
         dataset = gdal.Open(self.output_mrf+":MRF:Z1")
         driver = dataset.GetDriver()
@@ -557,6 +552,220 @@ class TestMRFGeneration_OBPG(unittest.TestCase):
         key_str = cur.fetchone()[0]
         print key_str
         self.assertEqual(key_str, '20151202000000', "Time for Z=1 does not match in ZDB")
+        if con:
+            con.close()
+        
+        print "\n***Test Case Passed***\n"
+        
+    def tearDown(self):
+        shutil.rmtree(self.working_dir)
+        shutil.rmtree(self.logfile_dir)
+        shutil.rmtree(self.output_dir)
+        
+class TestMRFGeneration_OBPG_webmerc(unittest.TestCase):
+    
+    def setUp(self):
+        self.dirpath = os.path.dirname(__file__)
+        self.test_config = self.dirpath + "/test/mrfgen_test_config5.xml"
+        self.input_dir = self.dirpath + "/test/obpg/"
+        self.output_dir = self.dirpath + "/test/output_dir"
+        self.working_dir = self.dirpath + "/test/working_dir"
+        self.logfile_dir = self.dirpath + "/test/logfile_dir"
+        self.output_mrf = self.output_dir+ "/OBPG_webmerc2015336_.mrf"
+        self.output_ppg = self.output_dir+ "/OBPG_webmerc2015336_.ppg"
+        self.output_idx = self.output_dir+ "/OBPG_webmerc2015336_.idx"
+        self.output_zdb = self.output_dir+ "/OBPG_webmerc2015336_.zdb"
+        self.output_img = self.output_dir+ "/OBPG_webmerc2015336_.png"
+        self.compare_img = self.dirpath + "/test/test_comp5.png"
+        if not os.path.exists(self.input_dir):
+            os.makedirs(self.input_dir)
+        if not os.path.exists(self.output_dir):
+            os.makedirs(self.output_dir)
+        if not os.path.exists(self.working_dir):
+            os.makedirs(self.working_dir)
+        if not os.path.exists(self.logfile_dir):
+            os.makedirs(self.logfile_dir)
+        
+        # download tiles
+        run_command('wget -r --no-parent --reject "index.html*" --cut-dirs=7 -nH -nc -q -T 60 -P ' + self.input_dir + ' http://oceancolor.gsfc.nasa.gov/BRS/MODISA/L2FRBRS/OC/LAC/2015/336/')
+            
+        # create copy of colormap
+        shutil.copy2(self.dirpath + "/test/colormaps/MODIS_Aqua_Chlorophyll_A.xml", self.working_dir)
+        
+        print "Generating global Web Mercator image with granules "
+        run_command("python mrfgen.py -c " + self.test_config)
+        run_command('gdal_translate -of PNG -outsize 1024 1024 ' + self.output_mrf+':MRF:Z0 ' + self.output_img)
+           
+    def test_generate_mrf_obpg_webmerc(self):
+        '''
+        This covers the following test cases:        
+            Test auto creation of empty MRF
+            Test using existing MRF with reprojection with z-level
+            Test non-blending of input images with transparency
+        '''
+        # Check MRF generation succeeded
+        self.assertTrue(os.path.isfile(self.output_mrf), "MRF generation failed")
+        
+        # Read MRF
+        dataset = gdal.Open(self.output_mrf)
+        driver = dataset.GetDriver()
+        print 'Driver:', str(driver.LongName)
+        self.assertEqual(str(driver.LongName), "Meta Raster Format", "Driver is not Meta Raster Format")
+
+        # This part of the test previously looked for a triplet of files in dataset.GetFileList().         
+        print 'Files: {0}, {1}'.format(self.output_ppg, self.output_idx)
+        self.assertTrue(os.path.isfile(self.output_ppg), "MRF PPG generation failed")
+        self.assertTrue(os.path.isfile(self.output_idx), "MRF IDX generation failed")
+        self.assertTrue(os.path.isfile(self.output_zdb), "MRF ZDB generation failed")
+        
+        print 'Projection:', str(dataset.GetProjection())
+        self.assertEqual(str(dataset.GetProjection()),'PROJCS["WGS 84 / Pseudo-Mercator",GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4326"]],PROJECTION["Mercator_1SP"],PARAMETER["central_meridian",0],PARAMETER["scale_factor",1],PARAMETER["false_easting",0],PARAMETER["false_northing",0],UNIT["metre",1,AUTHORITY["EPSG","9001"]],AXIS["X",EAST],AXIS["Y",NORTH],EXTENSION["PROJ4","+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs"],AUTHORITY["EPSG","3857"]]')
+        
+        print 'Size: ',dataset.RasterXSize,'x',dataset.RasterYSize, 'x',dataset.RasterCount
+        self.assertEqual(dataset.RasterXSize, 32768, "Size does not match")
+        self.assertEqual(dataset.RasterYSize, 32768, "Size does not match")
+        self.assertEqual(dataset.RasterCount, 1, "Number of bands do not match")
+        
+        geotransform = dataset.GetGeoTransform()
+        print 'Origin: (',geotransform[0], ',',geotransform[3],')'
+        self.assertEqual(geotransform[0], -20037508.34, "Origin does not match")
+        self.assertEqual(geotransform[3], 20037508.34, "Origin does not match")
+        print 'Pixel Size: (',geotransform[1], ',',geotransform[5],')'
+        self.assertEqual(float(geotransform[1]), 1222.992452392578116, "Pixel size does not match")
+        self.assertEqual(float(geotransform[5]), -1222.992452392578116, "Pixel size does not match")
+        
+        band = dataset.GetRasterBand(1)
+        print 'Overviews:', band.GetOverviewCount()
+        self.assertEqual(band.GetOverviewCount(), 7, "Overview count does not match")
+        
+        # Compare MRF
+        img = gdal.Open(self.output_img)
+        print 'Size: ',img.RasterXSize,'x',img.RasterYSize, 'x',img.RasterCount        
+        print "Comparing: " + self.output_img + " to " + self.compare_img
+        self.assertTrue(filecmp.cmp(self.output_img, self.compare_img), "Output composite image does not match")        
+        
+        img = None
+        
+        # Test ZDB
+        print "Checking " + self.output_zdb
+        con = sqlite3.connect(self.output_zdb)
+        cur = con.cursor()
+        # Check for existing key
+        cur.execute("SELECT COUNT(*) FROM ZINDEX;")
+        lid = int(cur.fetchone()[0])
+        print "Number of records: " + str(lid)
+        self.assertEqual(lid, 1, "Number of records not matching in ZDB")
+        # Check for matching keys
+        cur.execute("SELECT key_str FROM ZINDEX where z=0;")
+        key_str = cur.fetchone()[0]
+        print key_str
+        self.assertEqual(key_str, '20151202', "Time for Z=0 does not match in ZDB")
+        if con:
+            con.close()
+        
+        print "\n***Test Case Passed***\n"
+        
+    def tearDown(self):
+        shutil.rmtree(self.working_dir)
+        shutil.rmtree(self.logfile_dir)
+        shutil.rmtree(self.output_dir)
+
+class TestMRFGeneration_tiled_z(unittest.TestCase):
+    '''
+    This covers the following test cases:
+        Test using tiled images with Z-level        
+        Test MRF generation with date and time
+    '''
+    def setUp(self):
+        self.dirpath = os.path.dirname(__file__)
+        self.test_config = self.dirpath + "/test/mrfgen_test_config6.xml"
+        self.input_dir = self.dirpath + "/test/MORCR143LLDY/"
+        self.output_dir = self.dirpath + "/test/output_dir"
+        self.working_dir = self.dirpath + "/test/working_dir"
+        self.logfile_dir = self.dirpath + "/test/logfile_dir"
+        self.output_mrf = self.output_dir+ "/MORCR143LLDY2016024000000_.mrf"
+        self.output_pjg = self.output_dir+ "/MORCR143LLDY2016024000000_.pjg"
+        self.output_idx = self.output_dir+ "/MORCR143LLDY2016024000000_.idx"
+        self.output_zdb = self.output_dir+ "/MORCR143LLDY2016024000000_.zdb"
+        self.output_img = self.output_dir+ "/MORCR143LLDY2016024000000_.jpg"
+        self.compare_img = self.dirpath + "/test/test_comp6.jpg"
+        if not os.path.exists(self.input_dir):
+            os.makedirs(self.input_dir)
+        if not os.path.exists(self.output_dir):
+            os.makedirs(self.output_dir)
+        if not os.path.exists(self.working_dir):
+            os.makedirs(self.working_dir)
+        if not os.path.exists(self.logfile_dir):
+            os.makedirs(self.logfile_dir)
+
+        # Get sample input tiles
+        if os.path.isfile(self.input_dir + 'MODIS_Terra_CorrectedReflectance_TrueColor_0.tif') == False:
+            run_command('gdal_translate -of GTiff -projwin -90 45 -81 36 \'<GDAL_WMS><Service name="TMS"><ServerUrl>http://map1.vis.earthdata.nasa.gov/wmts-geo/MODIS_Terra_CorrectedReflectance_TrueColor/default/2016-01-24/EPSG4326_250m/${z}/${y}/${x}.jpg</ServerUrl></Service><DataWindow><UpperLeftX>-180.0</UpperLeftX><UpperLeftY>90</UpperLeftY><LowerRightX>396.0</LowerRightX><LowerRightY>-198</LowerRightY><TileLevel>8</TileLevel><TileCountX>2</TileCountX><TileCountY>1</TileCountY><YOrigin>top</YOrigin></DataWindow><Projection>EPSG:4326</Projection><BlockSizeX>512</BlockSizeX><BlockSizeY>512</BlockSizeY><BandsCount>3</BandsCount></GDAL_WMS>\' ' + self.input_dir + 'MODIS_Terra_CorrectedReflectance_TrueColor_0.tif')
+        if os.path.isfile(self.input_dir + 'MODIS_Terra_CorrectedReflectance_TrueColor_1.tif') == False:
+            run_command('gdal_translate -of GTiff -projwin -81 45 -72 36 \'<GDAL_WMS><Service name="TMS"><ServerUrl>http://map1.vis.earthdata.nasa.gov/wmts-geo/MODIS_Terra_CorrectedReflectance_TrueColor/default/2016-01-24/EPSG4326_250m/${z}/${y}/${x}.jpg</ServerUrl></Service><DataWindow><UpperLeftX>-180.0</UpperLeftX><UpperLeftY>90</UpperLeftY><LowerRightX>396.0</LowerRightX><LowerRightY>-198</LowerRightY><TileLevel>8</TileLevel><TileCountX>2</TileCountX><TileCountY>1</TileCountY><YOrigin>top</YOrigin></DataWindow><Projection>EPSG:4326</Projection><BlockSizeX>512</BlockSizeX><BlockSizeY>512</BlockSizeY><BandsCount>3</BandsCount></GDAL_WMS>\' ' + self.input_dir + 'MODIS_Terra_CorrectedReflectance_TrueColor_1.tif')
+            
+        #generate MRF
+        run_command("python mrfgen.py -c " + self.test_config)
+        run_command('gdal_translate -of JPEG -outsize 1024 512 -projwin -10018754.1713946 5621521.48619207 -8015003.3371157 4300621.37204427 ' + self.output_mrf+':MRF:Z0 ' + self.output_img)
+        
+    def test_generate_mrf(self):
+        # Check MRF generation succeeded
+        self.assertTrue(os.path.isfile(self.output_mrf), "MRF generation failed")
+        
+        # Read MRF
+        dataset = gdal.Open(self.output_mrf+":MRF:Z0")
+        driver = dataset.GetDriver()
+        print 'Driver:', str(driver.LongName)
+        self.assertEqual(str(driver.LongName), "Meta Raster Format", "Driver is not Meta Raster Format")
+        
+        # This part of the test previously looked for a triplet of files in dataset.GetFileList(). 
+        print 'Files: {0}, {1}'.format(self.output_pjg, self.output_idx)
+        self.assertTrue(os.path.isfile(self.output_pjg), "MRF PJG generation failed")
+        self.assertTrue(os.path.isfile(self.output_idx), "MRF IDX generation failed")
+        self.assertTrue(os.path.isfile(self.output_zdb), "MRF ZDB generation failed")
+        
+        print 'Projection:', str(dataset.GetProjection())
+        self.assertEqual(str(dataset.GetProjection()),'PROJCS["WGS 84 / Pseudo-Mercator",GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4326"]],PROJECTION["Mercator_1SP"],PARAMETER["central_meridian",0],PARAMETER["scale_factor",1],PARAMETER["false_easting",0],PARAMETER["false_northing",0],UNIT["metre",1,AUTHORITY["EPSG","9001"]],AXIS["X",EAST],AXIS["Y",NORTH],EXTENSION["PROJ4","+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs"],AUTHORITY["EPSG","3857"]]')
+        
+        print 'Size: ',dataset.RasterXSize,'x',dataset.RasterYSize, 'x',dataset.RasterCount
+        self.assertEqual(dataset.RasterXSize, 163840, "Size does not match")
+        self.assertEqual(dataset.RasterYSize, 163840, "Size does not match")
+        self.assertEqual(dataset.RasterCount, 3, "Size does not match")
+        
+        geotransform = dataset.GetGeoTransform()
+        print 'Origin: (',geotransform[0], ',',geotransform[3],')'
+        self.assertEqual(geotransform[0], -20037508.34, "Origin does not match")
+        self.assertEqual(geotransform[3], 20037508.34, "Origin does not match")
+        print 'Pixel Size: (',geotransform[1], ',',geotransform[5],')'
+        self.assertEqual(str(geotransform[1]), '244.598490479', "Pixel size does not match")
+        self.assertEqual(str(geotransform[5]), '-244.598490479', "Pixel size does not match")
+        
+        band = dataset.GetRasterBand(1)
+        print 'Overviews:', band.GetOverviewCount()
+        self.assertEqual(band.GetOverviewCount(), 10, "Overview count does not match")
+        
+        # Compare MRF
+        img = gdal.Open(self.output_img)
+        print 'Size: ',img.RasterXSize,'x',img.RasterYSize, 'x',img.RasterCount        
+        print "Comparing: " + self.output_img + " to " + self.compare_img
+        self.assertTrue(filecmp.cmp(self.output_img, self.compare_img), "Output composite image does not match")        
+        
+        img = None
+        
+        # Test ZDB
+        print "Checking " + self.output_zdb
+        con = sqlite3.connect(self.output_zdb)
+        cur = con.cursor()
+        # Check for existing key
+        cur.execute("SELECT COUNT(*) FROM ZINDEX;")
+        lid = int(cur.fetchone()[0])
+        print "Number of records: " + str(lid)
+        self.assertEqual(lid, 1, "Number of records not matching in ZDB")
+        # Check for matching keys
+        cur.execute("SELECT key_str FROM ZINDEX where z=0;")
+        key_str = cur.fetchone()[0]
+        print key_str
+        self.assertEqual(key_str, 'test', "Time for Z=0 does not match in ZDB")
         if con:
             con.close()
         
