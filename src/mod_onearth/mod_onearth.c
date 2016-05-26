00c4b1a495e76b59d785341a569c7884af1c90e8
Joe Roberts <joe.t.roberts@jpl.nasa.gov>
2016-05-25 17:02:15 -0700

    GIBS-1472 Added experimental direct z level support to WMTS REST request

--- src/mod_onearth/mod_onearth.c
+++ src/mod_onearth/mod_onearth.c
@@ -2196,7 +2196,7 @@ int rewrite_rest_uri(request_rec *r) {
 
 	int length = i-d;
 	// test using number of slashes
-	if (length < 7 || length > 8)
+	if (length < 7 || length > 9)
 		return -1;
 
 	i = i-1; // step back to split on "."
@@ -2210,7 +2210,9 @@ int rewrite_rest_uri(request_rec *r) {
 		r->args = apr_psprintf(r->pool,"wmts.cgi?SERVICE=%s&REQUEST=%s&VERSION=%s&LAYER=%s&STYLE=%s&TILEMATRIXSET=%s&TILEMATRIX=%s&TILEROW=%s&TILECOL=%s&FORMAT=image%%2F%s","WMTS","GetTile","1.0.0",params[1+d],params[2+d],params[3+d],params[4+d],params[5+d],params[6+d],ap_strcasecmp_match(params[7+d],"jpg") == 0 ? "jpeg" : params[7+d]);
 	if (length == 8)
 		r->args = apr_psprintf(r->pool,"wmts.cgi?SERVICE=%s&REQUEST=%s&VERSION=%s&LAYER=%s&STYLE=%s&TILEMATRIXSET=%s&TILEMATRIX=%s&TILEROW=%s&TILECOL=%s&FORMAT=image%%2F%s&TIME=%s","WMTS","GetTile","1.0.0",params[1+d],params[2+d],params[4+d],params[5+d],params[6+d],params[7+d],ap_strcasecmp_match(params[8+d],"jpg") == 0 ? "jpeg" : params[8+d],params[3+d]);
-//	ap_log_error(APLOG_MARK,APLOG_WARNING,0,r->server,"REST redirect -> %s/%s",r->uri,r->args);
+	if (length == 9)
+		r->args = apr_psprintf(r->pool,"wmts.cgi?SERVICE=%s&REQUEST=%s&VERSION=%s&LAYER=%s&STYLE=%s&TILEMATRIXSET=%s&TILEMATRIX=%s&TILEROW=%s&TILECOL=%s&FORMAT=image%%2F%s&TIME=%s&ZINDEX=%s","WMTS","GetTile","1.0.0",params[1+d],params[2+d],params[4+d],params[5+d],params[6+d],params[7+d],ap_strcasecmp_match(params[9+d],"jpg") == 0 ? "jpeg" : params[9+d],params[3+d],params[8+d]);
+	ap_log_error(APLOG_MARK,APLOG_WARNING,0,r->server,"REST redirect -> %s/%s",r->uri,r->args);
 	ap_internal_redirect(apr_psprintf(r->pool,"%s/%s",r->uri,r->args),r);
 	return 0;
 }
