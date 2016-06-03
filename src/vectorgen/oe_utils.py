293a10931c66f015d19e0c568451cf5d48ce203a
Joe Roberts <joe.t.roberts@jpl.nasa.gov>
2016-06-03 15:15:00 -0700

    GIBS-1452 Initial vectorgen tool with shapefile to geojson translator

---
+++ src/vectorgen/oe_utils.py
@@ -0,0 +1,306 @@
+# Copyright (c) 2002-2016, California Institute of Technology.
+# All rights reserved.  Based on Government Sponsored Research under contracts NAS7-1407 and/or NAS7-03001.
+#
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+#   1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+#   2. Redistributions in binary form must reproduce the above copyright notice,
+#      this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+#   3. Neither the name of the California Institute of Technology (Caltech), its operating division the Jet Propulsion Laboratory (JPL),
+#      the National Aeronautics and Space Administration (NASA), nor the names of its contributors may be used to
+#      endorse or promote products derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE CALIFORNIA INSTITUTE OF TECHNOLOGY BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+# EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+This file contains various utilities for the OnEarth tools.
+"""
+
+import glob
+import logging
+import os
+import subprocess
+import sys
+import time
+import datetime
+import socket
+import urllib
+import urllib2
+
+basename = None
+
+def sigevent(type, mssg, sigevent_url):
+    """
+    Send a message to sigevent service.
+    Arguments:
+        type -- 'INFO', 'WARN', 'ERROR'
+        mssg -- 'message for operations'
+        sigevent_url -- Example:  'http://[host]/sigevent/events/create'
+                        'http://localhost:8100/sigevent/events/create'
+    """
+    # Constrain mssg to 256 characters (including '...').
+    if len(mssg) > 256:
+        mssg=str().join([mssg[0:253], '...'])
+    print str().join(['sigevent ', type, ' - ', mssg])
+    # Remove any trailing slash from URL.
+    if sigevent_url[-1] == '/':
+        sigevent_url=sigevent_url[0:len(sigevent_url)-1]
+    # Remove any question mark from URL.  It is added later.
+    if sigevent_url[-1] == '?':
+        sigevent_url=sigevent_url[0:len(sigevent_url)-1]
+    # Remove any trailing slash from URL.  (Again.)
+    if sigevent_url[-1] == '/':
+        sigevent_url=sigevent_url[0:len(sigevent_url)-1]
+    # Define sigevent parameters that get encoded into the URL.
+    data={}
+    data['type']=type
+    data['description']=mssg
+    data['computer']=socket.gethostname()
+    data['source']='ONEARTH'
+    data['format']='TEXT'
+    data['category']='MRFGEN'
+    data['provider']='GIBS'
+    if basename != None:
+        data['data']=basename
+    # Format sigevent parameters that get encoded into the URL.
+    values=urllib.urlencode(data)
+    # Create complete URL.
+    full_url=sigevent_url+'?'+values
+    data=urllib2.urlopen(full_url)
+
+def log_info_mssg(mssg):
+    """
+    For information messages only.  Not for warning or error.
+    Arguments:
+        mssg -- 'message for operations'
+    """
+    # Send to log.
+    print mssg
+    logging.info(mssg)
+
+def log_info_mssg_with_timestamp(mssg):
+    """
+    For information messages only.  Not for warning or error.
+    Arguments:
+        mssg -- 'message for operations'
+    """
+    # Send to log.
+    print time.asctime()
+    logging.info(time.asctime())
+    log_info_mssg(mssg)
+
+def log_sig_warn(mssg, sigevent_url):
+    """
+    Send a warning to the log and to sigevent.
+    Arguments:
+        mssg -- 'message for operations'
+        sigevent_url -- Example:  'http://[host]/sigevent/events/create'
+    """
+    # Send to log.
+    logging.warning(time.asctime())
+    logging.warning(mssg)
+    # Send to sigevent.
+    try:
+        sent=sigevent('WARN', mssg, sigevent_url)
+    except urllib2.URLError:
+        print 'sigevent service is unavailable'
+        
+def log_sig_err(mssg, sigevent_url):
+    """
+    Send an error to the log and to sigevent.
+    Arguments:
+        mssg -- 'message for operations'
+        sigevent_url -- Example:  'http://[host]/sigevent/events/create'
+    """
+    # Send to log.
+    logging.warning(time.asctime())
+    logging.warning(mssg)
+    # Send to sigevent.
+    try:
+        sent=sigevent('ERROR', mssg, sigevent_url)
+    except urllib2.URLError:
+        print 'sigevent service is unavailable'
+
+def log_sig_exit(type, mssg, sigevent_url):
+    """
+    Send a message to the log, to sigevent, and then exit.
+    Arguments:
+        type -- 'INFO', 'WARN', 'ERROR'
+        mssg -- 'message for operations'
+        sigevent_url -- Example:  'http://[host]/sigevent/events/create'
+    """
+    exit_code = 0
+    # Add "Exiting" to mssg.
+    mssg=str().join([mssg, '  Exiting mrfgen.'])
+    # Send to sigevent.
+    try:
+        sent=sigevent(type, mssg, sigevent_url)
+    except urllib2.URLError:
+        print 'sigevent service is unavailable'
+    # Send to log.
+    if type == 'INFO':
+        log_info_mssg_with_timestamp(mssg)
+    elif type == 'WARN':
+        logging.warning(time.asctime())
+        logging.warning(mssg)
+    elif type == 'ERROR':
+        logging.error(time.asctime())
+        logging.error(mssg)
+        exit_code = 1
+    # Exit.
+    sys.exit(exit_code)
+
+def log_the_command(command_list):
+    """
+    Send a command list to the log.
+    Arguments:
+        command_list -- list containing all elements of a subprocess command.
+    """
+    # Add a blank space between each element.
+    spaced_command=''
+    for ndx in range(len(command_list)):
+        spaced_command=str().join([spaced_command, command_list[ndx], ' '])
+    # Send to log.
+    log_info_mssg_with_timestamp(spaced_command)
+
+def get_modification_time(filename):
+    """
+    Return (fake) floating point value of posix modification time for a file.
+    The (fake) floating point value is yyyymmdd.hhmmss which may be treated 
+    as a floating point number for the sake of time ordering.
+    Arguments:
+        filename -- name of file for which to return the modificaton time
+    """
+    # Get posix system time for mrf file to check the modification time.
+    stats=os.stat(filename)
+    # Convert st_mtime to time string "yyyymmdd.hhmmss".
+    addt=time.strftime('%Y%m%d.%H%M%S', time.localtime(stats.st_mtime))
+    mssg=str().join(['modification time ', addt, ' ', filename])
+    # Send to log.
+    log_info_mssg(mssg)
+    # Return time as string.
+    return addt
+
+def get_dom_tag_value(dom, tag_name):
+    """
+    Return value of a tag from dom (XML file).
+    Arguments:
+        tag_name -- name of dom tag for which the value should be returned.
+    """
+    tag=dom.getElementsByTagName(tag_name)
+    value=tag[0].firstChild.data.strip()
+    return value
+
+def remove_file(filename):
+    """
+    Delete a file or link, and report this action to the log.
+    Arguments:
+        filename -- file to remove.
+    """
+    preexisting=glob.glob(filename)
+    if len(preexisting) > 0:
+        # Send to log.
+        if os.path.islink(filename):
+            log_info_mssg(str().join(['Removing link:  ', filename]))
+        else:
+            log_info_mssg(str().join(['Removing file:  ', filename]))
+        os.remove(filename)
+
+def check_abs_path(directory_path):
+    """
+    Check if directory is absolute path.
+    If not, prepend current working directory.
+        Argument:
+            directory_path -- path to check if absolute
+    """
+    if directory_path[0] != '/':
+        directory_path = os.getcwd() +'/' + directory_path
+    
+    return directory_path
+    
+def add_trailing_slash(directory_path):
+    """
+    Add trailing slash if one is not already present.
+    Argument:
+        directory_path -- path to which trailing slash should be confirmed.
+    """
+    # Add trailing slash.
+    if directory_path[-1] != '/':
+        directory_path=str().join([directory_path, '/'])
+    # Return directory_path with trailing slash.
+    return directory_path
+
+def verify_directory_path_exists(directory_path, variable_name, sigevent_url):
+    """
+    Verify that directory_path exists.
+    Argument:
+        directory_path -- path whose existence needs to be verified.
+    """
+    if not os.path.isdir(directory_path):
+        mssg=str().join([variable_name, ' ', directory_path, 
+                         ' does not exist.'])
+        log_sig_exit('ERROR', mssg, sigevent_url)
+
+def get_input_files(dom):
+    """
+    Returns comma-separated list of files from <input_files> element.
+    Arguments:
+        dom -- The XML dom in which to retrieve <input_files> element.
+    """
+    files = []
+    input_file_element = dom.getElementsByTagName("input_files")[0]
+    file_elements = input_file_element.getElementsByTagName("file")
+    if len(file_elements) > 0:
+        for element in file_elements:
+            files.append(check_abs_path(element.firstChild.data.strip()))
+    else:
+        files.append(check_abs_path(input_file_element.firstChild.data.strip())) 
+    return ",".join(files)
+
+def get_doy_string(date_of_data):
+    """
+    Convert date_of_data string into three-digit doy string.
+    Argument:
+        date_of_data -- string variable, example: 20120730
+    """
+    y=int(date_of_data[0:4])
+    m=int(date_of_data[4:6])
+    d=int(date_of_data[6:8])
+    doy=str(datetime.datetime(y, m, d).timetuple().tm_yday)
+    if int(doy) < 10:
+        doy=str().join(['00', str(int(doy))])
+    elif int(doy) < 100:
+        doy=str().join(['0', str(int(doy))])
+    return doy
+
+def run_command(cmd, sigevent_url):
+    """
+    Runs the provided command on the terminal.
+    Arguments:
+        cmd -- the command to be executed.
+    """
+    log_info_mssg(' '.join(cmd))
+    process = subprocess.Popen(cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    process.wait()
+    for output in process.stdout:
+        log_info_mssg(output.strip())
+    for error in process.stderr:
+        log_sig_err(error.strip(), sigevent_url)
+        raise Exception(error.strip())
---
+++ src/vectorgen/vectorgen.py
@@ -0,0 +1,313 @@
+#!/bin/env python
+
+# Copyright (c) 2002-2016, California Institute of Technology.
+# All rights reserved.  Based on Government Sponsored Research under contracts NAS7-1407 and/or NAS7-03001.
+#
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+#   1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+#   2. Redistributions in binary form must reproduce the above copyright notice,
+#      this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+#   3. Neither the name of the California Institute of Technology (Caltech), its operating division the Jet Propulsion Laboratory (JPL),
+#      the National Aeronautics and Space Administration (NASA), nor the names of its contributors may be used to
+#      endorse or promote products derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE CALIFORNIA INSTITUTE OF TECHNOLOGY BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+# EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# Pipeline for converting vector-based datasets into standardized vector tiles, rasterized tiles, and GeoJSON.
+#
+# Example:
+#
+#  vectorgen.py 
+#   -c vectorgen_configuration_file.xml 
+#   -s http://localhost:8100/sigevent/events/create
+
+from optparse import OptionParser
+from oe_utils import *
+import glob
+import logging
+import os
+import sys
+import time
+import xml.dom.minidom
+import string
+import shutil
+try:
+    from osgeo import ogr, osr, gdal
+except:
+    sys.exit('ERROR: cannot find GDAL/OGR modules')
+
+versionNumber = '1.0.0'
+basename = None
+
+def shp2geojson(in_filename, out_filename, sigevent_url):
+    """
+    Converts Esri Shapefile into GeoJSON.
+    Arguments:
+        in_filename -- the input Shapefile
+        out_filename -- the output GeoJSON file
+        sigevent_url -- the URL for SigEvent
+    """
+    ogr2ogr_command_list = ['ogr2ogr', '-f', 'GeoJSON', out_filename, in_filename]
+    run_command(ogr2ogr_command_list, sigevent_url)
+
+if __name__ == '__main__':
+    
+    # Declare counter for errors
+    errors = 0
+    
+    # Define command line options and args.
+    parser = OptionParser(version = versionNumber)
+    parser.add_option('-c', '--configuration_filename',
+                      action='store', type='string', dest='configuration_filename',
+                      default='./vectorgen_configuration_file.xml',
+                      help='Full path of configuration filename.  Default:  ./vectorgen_configuration_file.xml')
+    parser.add_option("-d", "--data_only", action="store_true", dest="data_only", 
+                      default=False, help="Only output the MRF data, index, and header files")
+    parser.add_option('-s', '--sigevent_url',
+                      action='store', type='string', dest='sigevent_url',
+                      default=
+                      'http://localhost:8100/sigevent/events/create',
+                      help='Default:  http://localhost:8100/sigevent/events/create')
+    
+    # Read command line args.
+    (options, args) = parser.parse_args()
+    # Configuration filename.
+    configuration_filename=options.configuration_filename
+    # Sigevent URL.
+    sigevent_url=options.sigevent_url
+    # Data only.
+    data_only = options.data_only
+    
+    # Get current time, which is written to a file as the previous cycle time.  
+    # Time format is "yyyymmdd.hhmmss".  Do this first to avoid any gap where tiles 
+    # may get passed over because they were created while this script is running.
+    current_cycle_time=time.strftime('%Y%m%d.%H%M%S', time.localtime())
+    
+    # Read XML configuration file.
+    try:
+        # Open file.
+        config_file=open(configuration_filename, 'r')
+    except IOError:
+        mssg=str().join(['Cannot read configuration file:  ', configuration_filename])
+        log_sig_exit('ERROR', mssg, sigevent_url)
+    else:
+        # Get dom from XML file.
+        dom=xml.dom.minidom.parse(config_file)
+        # Parameter name.
+        parameter_name = get_dom_tag_value(dom, 'parameter_name')
+        date_of_data = get_dom_tag_value(dom, 'date_of_data')
+    
+        # Define output basename for log, txt, vrt, .mrf, .idx and .ppg or .pjg
+        # Files get date_of_date added, links do not.
+        basename=str().join([parameter_name, '_', date_of_data, '___', 'vectorgen_', 
+                             current_cycle_time])    
+        
+        # for sub-daily imagery
+        try: 
+            time_of_data = get_dom_tag_value(dom, 'time_of_data')
+        except:
+            time_of_data = ''
+        # Directories.
+        try:
+            input_dir = get_dom_tag_value(dom, 'input_dir')
+        except: 
+            input_dir = None
+        output_dir = get_dom_tag_value(dom, 'output_dir')
+        try:
+            working_dir = get_dom_tag_value(dom, 'working_dir')
+            working_dir = add_trailing_slash(check_abs_path(working_dir))
+        except: # use /tmp/ as default
+            working_dir ='/tmp/'
+        try:
+            logfile_dir = get_dom_tag_value(dom, 'logfile_dir')
+        except: #use working_dir if not specified
+            logfile_dir = working_dir
+        try:
+            output_name=get_dom_tag_value(dom, 'output_name')
+        except:
+            # default to GIBS naming convention
+            output_name='{$parameter_name}%Y%j_.json'
+        output_format = string.lower(get_dom_tag_value(dom, 'output_format'))
+        try:
+            outsize = get_dom_tag_value(dom, 'outsize')
+            target_x, target_y = outsize.split(' ')
+        except:
+            outsize = ''
+            try:
+                target_x = get_dom_tag_value(dom, 'target_x')
+            except:
+                target_x = '' # if no target_x then use rasterXSize and rasterYSize from VRT file
+            try:
+                target_y = get_dom_tag_value(dom, 'target_y')
+            except:
+                target_y = ''
+        # EPSG code projection.
+        try:
+            target_epsg = 'EPSG:' + str(get_dom_tag_value(dom, 'target_epsg'))
+        except:
+            target_epsg = 'EPSG:4326' # default to geographic
+        try:
+            source_epsg = 'EPSG:' + str(get_dom_tag_value(dom, 'source_epsg'))
+        except:
+            source_epsg = 'EPSG:4326' # default to geographic
+        # Target extents.
+        try:
+            extents = get_dom_tag_value(dom, 'extents')
+        except:
+            extents = '-180,-90,180,90' # default to geographic
+        xmin, ymin, xmax, ymax = extents.split(',')
+        try:
+            target_extents = get_dom_tag_value(dom, 'target_extents')
+        except:
+            if target_epsg == 'EPSG:3857':
+                target_extents = '-20037508.34,-20037508.34,20037508.34,20037508.34'
+            else:
+                target_extents = extents # default to extents
+        target_xmin, target_ymin, target_xmax, target_ymax = target_extents.split(',')
+        # Input files.
+        try:
+            input_files = get_input_files(dom)
+            if input_files == '':
+                raise ValueError('No input files provided')
+        except:
+            if input_dir == None:
+                log_sig_exit('ERROR', "<input_files> or <input_dir> is required", sigevent_url)
+            else:
+                input_files = ''
+
+        # Close file.
+        config_file.close()
+    
+    # Make certain each directory exists and has a trailing slash.
+    if input_dir != None:
+        input_dir = add_trailing_slash(check_abs_path(input_dir))
+    output_dir = add_trailing_slash(check_abs_path(output_dir))
+    logfile_dir = add_trailing_slash(check_abs_path(logfile_dir))
+    
+    # Save script_dir
+    script_dir = add_trailing_slash(os.path.dirname(os.path.abspath(__file__)))
+    
+    # Verify logfile_dir first so that the log can be started.
+    verify_directory_path_exists(logfile_dir, 'logfile_dir', sigevent_url)
+    # Initialize log file.
+    log_filename=str().join([logfile_dir, basename, '.log'])
+    logging.basicConfig(filename=log_filename, level=logging.INFO)
+    
+    # Verify remaining directory paths.
+    if input_dir != None:
+        verify_directory_path_exists(input_dir, 'input_dir', sigevent_url)
+    verify_directory_path_exists(output_dir, 'output_dir', sigevent_url)
+    verify_directory_path_exists(working_dir, 'working_dir', sigevent_url)
+    
+    # Log all of the configuration information.
+    log_info_mssg_with_timestamp(str().join(['config XML file:                ', configuration_filename]))                                      
+    # Copy configuration file to working_dir (if it's not already there) so that the output can be recreated if needed.
+    if os.path.dirname(configuration_filename) != os.path.dirname(working_dir):
+        config_preexisting=glob.glob(configuration_filename)
+        if len(config_preexisting) > 0:
+            at_dest_filename=str().join([working_dir, configuration_filename])
+            at_dest_preexisting=glob.glob(at_dest_filename)
+            if len(at_dest_preexisting) > 0:
+                remove_file(at_dest_filename)
+            shutil.copy(configuration_filename, working_dir+"/"+basename+".configuration_file.xml")
+            log_info_mssg(str().join([
+                              'config XML file copied to       ', working_dir]))
+    log_info_mssg(str().join(['config parameter_name:          ', parameter_name]))
+    log_info_mssg(str().join(['config date_of_data:            ', date_of_data]))
+    log_info_mssg(str().join(['config time_of_data:            ', time_of_data]))
+    if input_files != '':
+        log_info_mssg(str().join(['config input_files:             ', input_files]))
+    if input_dir != None:
+        log_info_mssg(str().join(['config input_dir:               ', input_dir]))
+    log_info_mssg(str().join(['config output_dir:              ', output_dir]))
+    log_info_mssg(str().join(['config working_dir:             ', working_dir]))
+    log_info_mssg(str().join(['config logfile_dir:             ', logfile_dir]))
+    log_info_mssg(str().join(['config output_name:             ', output_name]))
+    log_info_mssg(str().join(['config output_format:           ', output_format]))
+    log_info_mssg(str().join(['config outsize:                 ', outsize]))
+    log_info_mssg(str().join(['config target_x:                ', target_x]))
+    log_info_mssg(str().join(['config target_y:                ', target_y]))
+    log_info_mssg(str().join(['config target_epsg:             ', target_epsg]))
+    log_info_mssg(str().join(['config source_epsg:             ', source_epsg]))
+    log_info_mssg(str().join(['config extents:                 ', extents]))
+    log_info_mssg(str().join(['config target_extents:          ', target_extents]))
+    log_info_mssg(str().join(['vectorgen current_cycle_time:   ', current_cycle_time]))
+    log_info_mssg(str().join(['vectorgen basename:             ', basename]))
+    
+    # Verify that date is 8 characters.
+    if len(date_of_data) != 8:
+        mssg='Format for <date_of_data> (in vectorgen XML config file) is:  yyyymmdd'
+        log_sig_exit('ERROR', mssg, sigevent_url)
+        
+    if time_of_data != '' and len(time_of_data) != 6:
+        mssg='Format for <time_of_data> (in vectorgen XML config file) is:  HHMMSS'
+        log_sig_exit('ERROR', mssg, sigevent_url)
+    
+    # Change directory to working_dir.
+    os.chdir(working_dir)
+    
+    # Get list of all tile filenames.
+    alltiles = []
+    if input_files != '':
+        input_files = input_files.strip()
+        alltiles = input_files.split(',')
+    if input_dir != None:
+        if output_format == 'jpeg' or output_format == 'jpg':
+            alltiles = alltiles + glob.glob(str().join([input_dir, '*.jpg']))
+        else:
+            alltiles = alltiles + glob.glob(str().join([input_dir, '*.png']))
+        # check for tiffs
+        alltiles = alltiles + glob.glob(str().join([input_dir, '*.tif']))
+        alltiles = alltiles + glob.glob(str().join([input_dir, '*.tiff']))
+    
+    striptiles = []
+    for tile in alltiles:
+        striptiles.append(tile.strip())
+    alltiles = striptiles
+    
+    if len(time_of_data) == 6:
+        mrf_date = datetime.datetime.strptime(str(date_of_data)+str(time_of_data),"%Y%m%d%H%M%S")
+    else: 
+        mrf_date = datetime.datetime.strptime(date_of_data,"%Y%m%d")
+    out_filename = output_name.replace('{$parameter_name}', parameter_name)
+    time_params = []
+    for i, char in enumerate(out_filename):
+        if char == '%':
+            time_params.append(char+out_filename[i+1])
+    for time_param in time_params:
+        out_filename = out_filename.replace(time_param,datetime.datetime.strftime(mrf_date,time_param))
+    
+    out_filename = output_dir + out_filename
+            
+    if len(alltiles) > 0 and output_format == "geojson":
+        shp2geojson(alltiles[0], out_filename, sigevent_url)
+    else:
+        log_sig_exit('ERROR', "No valid input files found", sigevent_url)
+    
+    # Send to log.
+    mssg=str().join(['Output created:  ', out_filename])
+    try:
+        log_info_mssg(mssg)
+        sigevent('INFO', mssg, sigevent_url)
+    except urllib2.URLError:
+        None
+    sys.exit(errors)
