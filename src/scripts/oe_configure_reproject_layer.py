4394b956e6543ab7bb10f0d49217be437752a78d
Joe Roberts <joe.t.roberts@jpl.nasa.gov>
2017-07-18 11:00:28 -0700

    ONEARTH-427 Fixed exceptions when reproject endpoints are not defined

--- src/scripts/oe_configure_reproject_layer.py
+++ src/scripts/oe_configure_reproject_layer.py
@@ -145,14 +145,23 @@ def build_reproject_configs(layer_config_path, tilematrixsets_config_path, wmts=
         log_sig_warn(mssg, sigevent_url)
 
     if len(environment_xml.findall('ReprojectEndpoint')) > 0:
-        wmts_reproject_endpoint = next(elem.text for elem in environment_xml.findall('ReprojectEndpoint') if elem.get('service') == 'wmts')
-        twms_reproject_endpoint = next(elem.text for elem in environment_xml.findall('ReprojectEndpoint') if elem.get('service') == 'twms')
+        try:
+            wmts_reproject_endpoint = next(elem.text for elem in environment_xml.findall('ReprojectEndpoint') if elem.get('service') == 'wmts')
+        except StopIteration:
+            wmts_reproject_endpoint = None
+        try:
+            twms_reproject_endpoint = next(elem.text for elem in environment_xml.findall('ReprojectEndpoint') if elem.get('service') == 'twms')
+        except StopIteration:
+            twms_reproject_endpoint = None
         if not wmts_reproject_endpoint:
-            mssg = 'no wmts reproject endpoint specified'
+            mssg = 'no wmts reproject endpoint specified in ' + environment_config_path
             errors.append(asctime() + " " + mssg)
-            log_sig_err(mssg, sigevent_url)
+            if twms: # TWMS requires the WMTS reproject endpoint to be defined
+                log_sig_exit('ERROR', mssg, sigevent_url)
+            else:
+                log_sig_err(mssg, sigevent_url)
         if not twms_reproject_endpoint:
-            mssg = 'no twms reproject endpoint specified'
+            mssg = 'no twms reproject endpoint specified in ' + environment_config_path
             errors.append(asctime() + " " + mssg)
             log_sig_err(mssg, sigevent_url)
     else:
@@ -438,7 +447,10 @@ def build_reproject_configs(layer_config_path, tilematrixsets_config_path, wmts=
         
         if wmts:
             # Finish building the layer Apache config
-            layer_apache_config = 'Alias {0}{1} {2}\n'.format(add_trailing_slash(wmts_reproject_endpoint), identifier, layer_endpoint)
+            if wmts_reproject_endpoint is not None:
+                layer_apache_config = 'Alias {0}{1} {2}\n'.format(add_trailing_slash(wmts_reproject_endpoint), identifier, layer_endpoint)
+            else:
+                layer_apache_config = ''
             layer_apache_config += '<Directory {0}>\n'.format(layer_endpoint)
             layer_apache_config += '\tWMTSWrapperRole layer\n'
             layer_apache_config += '\tWMTSWrapperEnableTime on\n'
@@ -458,7 +470,10 @@ def build_reproject_configs(layer_config_path, tilematrixsets_config_path, wmts=
                 log_sig_exit('ERROR', 'Cannot write layer config file: ' + layer_apache_config_path, sigevent_url)
 
             # Create final Apache configs (WMTS)
-            endpoint_apache_config = 'Alias {0}wmts.cgi {1}wmts.cgi\n'.format(add_trailing_slash(wmts_reproject_endpoint), add_trailing_slash(wmts_base_endpoint))
+            if wmts_reproject_endpoint is not None:
+                endpoint_apache_config = 'Alias {0}wmts.cgi {1}wmts.cgi\n'.format(add_trailing_slash(wmts_reproject_endpoint), add_trailing_slash(wmts_base_endpoint))
+            else:
+                endpoint_apache_config = ''
             endpoint_apache_config += '<Directory {0}>\n'.format(wmts_base_endpoint)
             endpoint_apache_config += '\tWMTSWrapperRole root\n'
             endpoint_apache_config += '</Directory>\n'
@@ -702,7 +717,10 @@ def build_reproject_configs(layer_config_path, tilematrixsets_config_path, wmts=
     if twms:
         # Create final Apache configs (TWMS)
         twms_apache_conf_path_template = os.path.join(twms_base_endpoint, '${layer}/twms.config')
-        twms_endpoint_apache_config = 'Alias {0} {1}\n'.format(twms_reproject_endpoint, twms_base_endpoint)
+        if twms_reproject_endpoint is not None:
+            twms_endpoint_apache_config = 'Alias {0} {1}\n'.format(twms_reproject_endpoint, twms_base_endpoint)
+        else:
+            twms_endpoint_apache_config = ''
         twms_endpoint_apache_config += '<Directory {0}>\n'.format(twms_base_endpoint)
         twms_endpoint_apache_config += '\ttWMS_RegExp twms.cgi\n'
         twms_endpoint_apache_config += '\ttWMS_ConfigurationFile {0}\n'.format(twms_apache_conf_path_template)
