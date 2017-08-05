c85a1cf83761db41ca2b0ebbbc4737e7dcc6a464
Marc Huffnagle <marc@element84.com>
2017-08-05 11:51:58 -0400

    Check for RPMs before building Docker image

--- bin/build_el7_docker_image.sh
+++ bin/build_el7_docker_image.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-set -evx
+set -e
+
+if ! ls dist/gibs-gdal-*.el7.*.rpm >/dev/null 2>&1; then
+  echo "No RPMs found in ./dist/" >&2
+  exit 1
+fi
 
 SCRIPT_NAME=$(basename "$0")
 TAG="$1"
@@ -19,9 +24,9 @@ echo "FROM $(cat docker/el7/gibs-gdal-image.txt)" > tmp/docker/Dockerfile
 grep -Ev '^FROM' docker/el7/Dockerfile >> tmp/docker/Dockerfile
 
 (
-  set -evx
+  set -e
   cd tmp/docker
-  docker build -t "$TAG" .
-)
+  docker build -t "$TAG" .)
+
 
 rm -rf tmp/docker
