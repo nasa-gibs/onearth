fb904f7a5fdbcbe263382327f23f7f8ce4de01db
Marc Huffnagle <marc@element84.com>
2017-08-22 11:00:06 -0400

    Reduce size of image

--- bin/build_el7_docker_image.sh
+++ bin/build_el7_docker_image.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if ! ls dist/gibs-gdal-*.el7.*.rpm >/dev/null 2>&1; then
+if ! ls dist/onearth-*.el7.*.rpm >/dev/null 2>&1; then
   echo "No RPMs found in ./dist/" >&2
   exit 1
 fi
@@ -18,6 +18,12 @@ fi
 rm -rf tmp/docker
 mkdir -p tmp/docker/rpms
 cp dist/onearth-*.el7.*.rpm tmp/docker/rpms/
+rm -f \
+  tmp/docker/rpms/onearth-debuginfo-*.el7.centos.x86_64.rpm \
+  tmp/docker/rpms/onearth-demo-*.el7.centos.x86_64.rpm \
+  tmp/docker/rpms/onearth-mapserver-*.el7.centos.x86_64.rpm \
+  tmp/docker/rpms/onearth-test-*.el7.centos.x86_64.rpm \
+  tmp/docker/rpms/onearth-vectorgen-*.el7.centos.x86_64.rpm
 cp docker/el7/run-onearth.sh tmp/docker/run-onearth.sh
 
 echo "FROM $(cat docker/el7/gibs-gdal-image.txt)" > tmp/docker/Dockerfile
@@ -26,7 +32,7 @@ grep -Ev '^FROM' docker/el7/Dockerfile >> tmp/docker/Dockerfile
 (
   set -e
   cd tmp/docker
-  docker build -t "$TAG" .)
-
+  docker build -t "$TAG" .
+)
 
 rm -rf tmp/docker
