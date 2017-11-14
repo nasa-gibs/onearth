7cf10810750c7e04c666458a86fd7ef694fe04ab
Joshua D Rodriguez <joshua.d.rodriguez@jpl.nasa.gov>
2017-11-14 13:58:50 -0800

    Added layer setup to Dockerfile plus startup script

---
+++ src/cloud_build/start_oe2.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ ! -f /.dockerenv ]; then
+  echo "This script is only intended to be run from within Docker" >&2
+  exit 1
+fi
+
+echo 'Starting Redis server'
+/usr/bin/redis-server &
+
+echo 'Starting Apache server'
+/usr/sbin/apachectl
+
+# Tail the apache logs
+exec tail -qF \
+  /etc/httpd/logs/access.log \
+  /etc/httpd/logs/error.log \
+  /etc/httpd/logs/access_log \
+  /etc/httpd/logs/error_log
