f2e3e843308d8887de78b2b354cc6e583900a0d8
Joe Roberts <joe.t.roberts@jpl.nasa.gov>
2018-06-27 18:07:42 -0700

    Fixed docker image builds for CI

--- bin/build_el7_demo_docker_image.sh
+++ bin/build_el7_demo_docker_image.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if ! ls dist/onearth-demo-*.el7.centos.noarch.rpm >/dev/null 2>&1; then
+if ! ls dist/onearth-demo-*.el7.noarch.rpm >/dev/null 2>&1; then
   echo "onearth-demo RPM not found in ./dist/" >&2
   exit 1
 fi
@@ -18,7 +18,7 @@ fi
 
 rm -rf tmp/docker
 mkdir -p tmp/docker/rpms
-cp dist/onearth-demo-*.el7.centos.noarch.rpm tmp/docker/rpms/
+cp dist/onearth-demo-*.el7.noarch.rpm tmp/docker/rpms/
 
 echo "FROM ${BASE_IMAGE}" > tmp/docker/Dockerfile
 egrep -v '^FROM ' < docker/el7-demo/Dockerfile >> tmp/docker/Dockerfile
