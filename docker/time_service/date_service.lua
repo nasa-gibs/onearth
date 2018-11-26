39ccf87392b36fe52e105054f479ba809837fee5
Joe Roberts <joe.t.roberts@jpl.nasa.gov>
2018-11-26 01:41:35 -0800

    GITC-937 Fixed time service lua so that it can use env variable for host

--- docker/time_service/date_service.lua
+++ docker/time_service/date_service.lua
@@ -1,2 +1,2 @@
 local onearthTimeService = require "onearthTimeService"
-handler = onearthTimeService.timeService({handler_type="redis", host="127.0.0.1"})
+handler = onearthTimeService.timeService({handler_type="redis", host="{REDIS_HOST}"}, {filename_format="hash"})
