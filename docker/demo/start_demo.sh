332fb9fc193894544a4cbbe3e9e9080ec4128d39
Joe Roberts <joe.t.roberts@jpl.nasa.gov>
2020-03-26 17:21:09 -0700

    GITC-1342 Updated demo container to output httpd log streams

---
+++ docker/demo/start_demo.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+DEBUG_LOGGING=${1:-false}
+
+if [ ! -f /.dockerenv ]; then
+  echo "This script is only intended to be run from within Docker" >&2
+  exit 1
+fi
+
+# Set Apache logs to debug log level
+if [ "$DEBUG_LOGGING" = true ]; then
+    perl -pi -e 's/LogLevel warn/LogLevel debug/g' /etc/httpd/conf/httpd.conf
+fi
+perl -pi -e 's/LogFormat "%h %l %u %t \\"%r\\" %>s %b/LogFormat "%h %l %u %t \\"%r\\" %>s %b %D/g' /etc/httpd/conf/httpd.conf
+
+echo 'Starting Apache server'
+/usr/sbin/httpd -k start
+sleep 2
+
+# Tail the apache logs
+exec tail -qFn 10000 \
+  /etc/httpd/logs/access.log \
+  /etc/httpd/logs/error.log \
+  /etc/httpd/logs/access_log \
+  /etc/httpd/logs/error_log
