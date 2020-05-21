816f386193e5f7a18acc973b84a96e0428022478
Matthew Cechini <matthew.f.cechini@nasa.gov>
2020-05-21 08:34:09 -0400

    Supporting `NNB` as an input value for overview_resampling

--- src/mrfgen/mrfgen.py
+++ src/mrfgen/mrfgen.py
@@ -1944,7 +1944,7 @@ remove_file(vrt_filename)
 
 # Check if this is an MRF insert update, if not then regenerate a new MRF
 mrf_list = []
-if overview_resampling[:4].lower() == 'near':
+if overview_resampling[:4].lower() == 'near' or overview_resampling == 'NNb':
     insert_method = 'NNb'
 else:
     insert_method = 'Avg'
