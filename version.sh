08e3e7781a61d62e623d17fe703709b8b6b3ad67
Joe Roberts <joe.t.roberts@jpl.nasa.gov>
2021-09-14 00:23:57 -0700

    Bumping to v2.5.0 for vector WMS updates

--- version.sh
+++ version.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # The onearth version to use in the Docker image tag (e.g. onearth-wms:{ONEARTH_VERSION}-{ONEARTH_RELEASE}).
-export ONEARTH_VERSION=2.4.1 # This must be manually incremented with each OnEarth version
+export ONEARTH_VERSION=2.5.0 # This must be manually incremented with each OnEarth version
 
 # The onearth release to use in the Docker image tag (e.g. onearth-wms:{ONEARTH_VERSION}-{ONEARTH_RELEASE}).
 export ONEARTH_RELEASE=1 # This must be manually incremented with each OnEarth build
