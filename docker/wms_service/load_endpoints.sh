d26029de333fb82d057ac21d112d73560c9159ed
Joe Roberts <joe.t.roberts@jpl.nasa.gov>
2021-09-23 00:22:48 -0700

    Run oe2_wms_configure with no shapefile_bucket when not provided.
    Apparently a blank env var isn't recognized with parallel

--- docker/wms_service/load_endpoints.sh
+++ docker/wms_service/load_endpoints.sh
@@ -2,6 +2,10 @@
 
 echo "[$(date)] Beginning WMS endpoint configuration..." >> /var/log/onearth/config.log
 
-grep -l mapserver /etc/onearth/config/endpoint/*.yaml | parallel -j 4 python3.6 /usr/bin/oe2_wms_configure.py {} --shapefile_bucket "${SHAPEFILE_BUCKET}" >> /var/log/onearth/config.log 2>&1
-
+if [ -z "$SHAPEFILE_BUCKET" ]
+then
+	grep -l mapserver /etc/onearth/config/endpoint/*.yaml | parallel -j 4 python3.6 /usr/bin/oe2_wms_configure.py {} >> /var/log/onearth/config.log 2>&1
+else
+	grep -l mapserver /etc/onearth/config/endpoint/*.yaml | parallel -j 4 python3.6 /usr/bin/oe2_wms_configure.py {} --shapefile_bucket "${SHAPEFILE_BUCKET}" >> /var/log/onearth/config.log 2>&1
+fi
 echo "[$(date)] Completed WMS endpoint configuration" >> /var/log/onearth/config.log
