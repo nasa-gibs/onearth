c31b9800a067de7493358c9324110fd16f734579
Daniel Chou <daniel.t.chou@jpl.nasa.gov>
2022-07-20 13:26:49 -0700

    revert to greyscale to asDirect

--- src/modules/mod_wmts_wrapper/configure_tool/oe2_reproject_configure.py
+++ src/modules/mod_wmts_wrapper/configure_tool/oe2_reproject_configure.py
@@ -346,7 +346,7 @@ def get_layer_bands(identifier, mimetype, sample_tile_url):
                 print(identifier + ' contains palette')
         except png.FormatError:
             # No palette, check for greyscale
-            if sample_png_read_info['greyscale'] is True:
+            if sample_png.asDirect()[3]['greyscale'] is True:
                 bands = 1
                 print(identifier + ' is greyscale')
             else:  # Check for alpha
