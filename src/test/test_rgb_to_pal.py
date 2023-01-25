784c4c41225b374415c43702b49eb431f79f3e9c
Joe Roberts <joe.t.roberts@jpl.nasa.gov>
2023-01-25 15:54:17 -0800

    GITC-4100 Fixed test_rgb_to_pal.py

--- src/test/test_rgb_to_pal.py
+++ src/test/test_rgb_to_pal.py
@@ -4,9 +4,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+
 # http://www.apache.org/licenses/LICENSE-2.0
-# 
+
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,7 +17,6 @@
 
 import os
 import sys
-import glob
 import subprocess
 import unittest2 as unittest
 import xmlrunner
@@ -27,11 +26,12 @@ from optparse import OptionParser
 from oe_test_utils import make_dir_tree
 import xml.etree.ElementTree as xmlet
 
+
 def commonFunction(self, fileName):
     try:
-        config_file=open(fileName, 'r')
+        config_file = open(fileName, 'r')
     except Exception as e:
-        print('Cannot open configuration file ' + filename + ': ' + e)
+        print('Cannot open configuration file ' + fileName + ': ' + e)
         sys.exit(-1)
 
     # Make XML tree.
@@ -75,23 +75,25 @@ def commonFunction(self, fileName):
     # Test code.
     if DEBUG:
         print('Compare: ' + outputCode + ' to ' + compareCode)
-    self.assertEqual(outputCode, compareCode, "Code doesn't match for: " + fileName )
-        
+    self.assertEqual(outputCode, compareCode, "Code doesn't match for: " + fileName)
+
     # Test image.
     if os.path.exists(outputImg):
         if DEBUG:
             print("Compare: " + outputImg + " to " + compareImg)
         self.assertTrue(filecmp.cmp(outputImg, compareImg), "Image doesn't match for: " + fileName)
 
+
 DEBUG = False
 SAVE_RESULTS = False
+
 
 class TestRgbToPal(unittest.TestCase):
 
     @classmethod
     def setUpClass(self):
 
-        self.testDir=os.getcwd()
+        self.testDir = os.getcwd()
 
         # Input configuration file directory.
         self.configPath = self.testDir + '/rgb_to_pal_files/config'
@@ -109,30 +111,39 @@ class TestRgbToPal(unittest.TestCase):
     def test_large_image(self):
         fileName = self.configPath + '/rgbtopal_test_config1.xml'
         commonFunction(self, fileName)
+
     def test_small_image(self):
         fileName = self.configPath + '/rgbtopal_test_config2.xml'
         commonFunction(self, fileName)
+
     def test_rgbapal_image(self):
         fileName = self.configPath + '/rgbtopal_test_config3.xml'
         commonFunction(self, fileName)
+
     def test_rgb_image_one_missing_color(self):
         fileName = self.configPath + '/rgbtopal_test_config4.xml'
         commonFunction(self, fileName)
+
     def test_small_image_one_missing_color(self):
         fileName = self.configPath + '/rgbtopal_test_config5.xml'
         commonFunction(self, fileName)
+
     def test_small_image_multiple_missing_colors(self):
         fileName = self.configPath + '/rgbtopal_test_config6.xml'
         commonFunction(self, fileName)
+
     def test_small_image_no_matching_color(self):
         fileName = self.configPath + '/rgbtopal_test_config7.xml'
         commonFunction(self, fileName)
+
     def test_small_image_mismatched_transparency(self):
         fileName = self.configPath + '/rgbtopal_test_config8.xml'
         commonFunction(self, fileName)
+
     def test_small_image_invalid_fill_value(self):
         fileName = self.configPath + '/rgbtopal_test_config9.xml'
         commonFunction(self, fileName)
+
     def test_geotiff_image(self):
         fileName = self.configPath + '/rgbtopal_test_config10.xml'
         commonFunction(self, fileName)
@@ -144,6 +155,7 @@ class TestRgbToPal(unittest.TestCase):
         else:
             print("Outputs in : " + self.outputPath)
 
+
 if __name__ == '__main__':
 
     print('Start test_rgb_to_pal.')
@@ -153,7 +165,13 @@ if __name__ == '__main__':
     }
 
     parser = OptionParser()
-    parser.add_option('-o', '--output', action='store', type='string', dest='result', help='Result file')
+    parser.add_option('-o',
+                      '--output',
+                      action='store',
+                      type='string',
+                      dest='result',
+                      default='test_rgb_to_pal_results.xml',
+                      help='Result file')
     (options, args) = parser.parse_args()
 
     suite = unittest.TestSuite()
@@ -166,4 +184,3 @@ if __name__ == '__main__':
         test_runner.run(suite)
 
     print('Complete test_rgb_to_pal.')
-
