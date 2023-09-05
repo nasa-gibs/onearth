a7a2399c744ed8b48f981686857c9bcb2ab0171a
Matthew Graber <matthew.w.graber@jpl.nasa.gov>
2023-09-05 15:22:45 -0700

    Added logic to handle 1-band no-alpha PNGs

--- src/modules/mod_wmts_wrapper/configure_tool/oe2_reproject_configure.py
+++ src/modules/mod_wmts_wrapper/configure_tool/oe2_reproject_configure.py
@@ -347,8 +347,13 @@ def get_layer_bands(identifier, mimetype, sample_tile_url):
         except png.FormatError:
             # No palette, check for greyscale
             if sample_png.asDirect()[3]['greyscale'] is True:
-                bands = 2
-                print(identifier + ' is greyscale')
+                print("readinfo", sample_png_read_info)
+                if sample_png_read_info['alpha'] is True:
+                    bands = 2
+                    print(identifier + ' is greyscale and alpha')
+                else:
+                    bands = 1
+                    print(identifier + ' is greyscale')
             else:  # Check for alpha
                 if sample_png_read_info['alpha'] is True:
                     bands = 4
