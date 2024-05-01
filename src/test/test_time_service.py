718e38a138b8937998e5c4c57703cea5f45ad20e
Matthew Graber <matthew.w.graber@jpl.nasa.gov>
2024-05-01 16:44:15 -0700

    Removed debug code from test_time_service.py

--- src/test/test_time_service.py
+++ src/test/test_time_service.py
@@ -1315,8 +1315,6 @@ class TestDateService(unittest.TestCase):
 
     @classmethod
     def tearDownClass(self):
-        with open("/var/log/httpd/error_log") as f:
-            print("yeet", f.read())
         if not DEBUG:
             os.remove(self.test_config_dest_path)
             os.remove(self.test_lua_config_location)
