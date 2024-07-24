803cb28621f0a366462eb1885bf52d8164a3e40c
jackiryan <Jacqueline.Ryan@jpl.nasa.gov>
2024-07-24 13:31:26 -0700

    Updated Colormap code to address comments, added round to floor mode

--- src/colormaps/bin/colorMaptoTXT.py
+++ src/colormaps/bin/colorMaptoTXT.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import argparse
-import dataclasses
-import re
-from typing import List, Optional
+from typing import List, Optional, Tuple
 from xml.dom import minidom
 
+# This is not -sys.float_info.min, but it works for this application.
+FLOAT_LOW_BOUND = "-100000000000000000000000000000000000000000000000000000000000000000000000000000"
+FLOAT_HIGH_BOUND = "100000000000000000000000000000000000000000000000000000000000000000000000000000"
 
 class XmlParseError(Exception):
     pass
@@ -22,10 +23,13 @@ def convertEntryValue(
     :param scale: An optional float specifying the scale factor to multiply by the cmap values.
     :param offset: An optional float specifying an amount to add to the cmap values.
     :param offset: The assumed output unit, or one explicitly specified via an input argument.
+
+    :returns: A string containing the scaled and offset colormap quantization level.
     '''
     optionalScale: float = scale or 1.0
     optionalOffset: float = offset or 0.0
     return str(optionalScale * float(entryValue) + optionalOffset)
+
 
 def parseColorMapEntryElement(
         cmapEntryElement: minidom.Element,
@@ -33,13 +37,13 @@ def parseColorMapEntryElement(
         offset: Optional[float]) -> str:
     '''
     Parse a <ColorMapEntry> tag.
-    TO DO: What are we supposed to with label attributes for Classification type colormaps?
 
     :param colorMapElement: A minidom.Element called "ColorMapEntry" that contains the required
     attributes "rgb" and "transparent" at a minimum.
     :param scale: An optional float specifying the scale factor to multiply by the cmap values.
     :param offset: An optional float specifying an amount to add to the cmap values.
-    :return: A single line str containing the parsed colormap entry in GDAL txt format.
+
+    :returns: A single line str containing the parsed colormap entry in GDAL txt format.
     '''
     # Throw KeyError if any of these required attributes are not found in the <ColorMapEntry>
     entryRgb: str = cmapEntryElement.attributes["rgb"].value
@@ -73,17 +77,18 @@ def parseColorMapEntryElement(
         # Handle INF: If the lower bound is -INF, use a value of -9999. This does not seem
         # robust, but is based off of the example outputs in layer-configs.
         if lineValue.upper() == "-INF":
-            lineValue = "-9999"
+            #lineValue = "-9999"
+            lineValue = FLOAT_LOW_BOUND
         elif lineValue.upper() == "INF":
             # Technically this branch should never be reached, since the lower bound is always used.
-            lineValue = "9999"
+            lineValue = FLOAT_HIGH_BOUND
         else:
             # Handle units, idempotent if both scale and offset are None.
             # Note that INF has to be handled first because this function casts to float.
             lineValue = convertEntryValue(lineValue, scale, offset)
 
-        # Always represent floating point values with two decimal places. Causes -9999 to become -9999.00
-        lineValue = f"{float(lineValue):.2f}"
+            # Always represent floating point values with two decimal places, unless -INF
+            lineValue = f"{float(lineValue):.2f}"
         
     lineRgb: str = entryRgb.replace(",", " ")
 
@@ -96,7 +101,8 @@ def parseColorMapEntryElement(
 def parseColorMapElement(
         colorMapElement: minidom.Element,
         scale: Optional[float],
-        offset: Optional[float]) -> str:
+        offset: Optional[float],
+        round: bool = False) -> str:
     '''
     Parse a <ColorMap> tag.
 
@@ -104,27 +110,48 @@ def parseColorMapElement(
     tags as children (may not be direct children).
     :param scale: An optional float specifying the scale factor to multiply by the cmap values.
     :param offset: An optional float specifying an amount to add to the cmap values.
-    :return: A str containing the parsed colormap entries in GDAL txt format.
+    :param round: Output the colormap in the round to floor mode if True.
+    
+    :returns: A str containing the parsed colormap entries in GDAL txt format.
+
+    :raises XmlParseError: If no ColorMapEntry tags are present in the input colormap XML.
     '''
     cmapEntryNodes: List[minidom.Element] = colorMapElement.getElementsByTagName("ColorMapEntry")
     if len(cmapEntryNodes) == 0:
         raise XmlParseError(f"No elements called \"ColorMapEntry\" were found in the document.")
 
+    #minValue, maxValue = findMinMaxValues(cmapEntryNodes)
+
     cmapStr: str = ""
+    cmapLinePrev: str = ""
     for entryNode in cmapEntryNodes:
-        cmapStr += parseColorMapEntryElement(
+        cmapLine: str = parseColorMapEntryElement(
             entryNode,
             scale,
             offset
         )
+
+        # If using the "round to the floor" mode, use a small offset + the previous line's value as the "floor"
+        # the next colormap quantization value. The R, G, B at this level has to come from the most recently
+        # parsed line.
+        if round and cmapLinePrev != "":
+            cmapLinePrevVal: str = cmapLinePrev.split()[0]
+            cmapLineRgb: str = " ".join(cmapLine.split()[1:])
+            # Do not add a "floor" line for colormap lines that are no data or -INF
+            if cmapLinePrevVal != "nv" and cmapLinePrevVal != FLOAT_LOW_BOUND:
+                cmapLineFloor = f"{(float(cmapLinePrevVal) + 0.001):.3f} {cmapLineRgb}\n"
+                cmapStr += cmapLineFloor
+        cmapStr += cmapLine
+        cmapLinePrev = cmapLine
 
     return cmapStr
 
 
 def parseColorMapDoc(
         colormapFile: str,
-        scale: Optional[str],
-        offset: Optional[str]) -> str:
+        scale: Optional[float],
+        offset: Optional[float],
+        round: bool = False) -> str:
     '''
     Parses input colormap XML document (a string referencing a filepath) using the xml.dom.minidom
     module and converts it to a text format. The text format has the following specification:
@@ -137,12 +164,17 @@ def parseColorMapDoc(
     :param colormap_file: A string filepath to the colormap XML document input.
     :param scale: An optional float specifying the scale factor to multiply by the cmap values.
     :param offset: An optional float specifying an amount to add to the cmap values.
-    :return: A string containing the parsed colormap information.
+    :param round: Output the colormap in the round to floor mode if True.
+
+    :returns: A string containing the parsed colormap information.
+
+    :raises ValueError: If either the user-provided scale or offset are unable to be cast to float.
+    :raises XmlParseError:  If no ColorMap tags are present in the input colormap XML.
     '''
-    # Input sanitization
+    # Input sanitization, rely on error handling in minidom.parse for the colormapFile argument.
     try:
-        parsedScale = float(scale) if scale is not None else None
-        parsedOffset = float(offset) if offset is not None else None
+        parsedScale: Optional[float] = float(scale) if scale is not None else None
+        parsedOffset: Optional[float] = float(offset) if offset is not None else None
     except ValueError as e:
         print(f"Provided input parameters are invalid, scale: {scale}, offset: {offset}")
         raise e
@@ -164,7 +196,8 @@ def parseColorMapDoc(
         colorMapLines: str = parseColorMapElement(
             cmapNode,
             parsedScale,
-            parsedOffset
+            parsedOffset,
+            round
         )
 
         # Use this janky heuristic to decide if the parsed colormap was the nodata one.
@@ -187,15 +220,15 @@ def colorMapToTxt(
     Top-level function for converting a colormap file into a text file compatible with GDAL.
 
     :param args: An argparse namespace containing the arguments from the CLI.
-    :return: None, a .txt file representing the colormap information is created as a side effect.
+
+    :returns: None, the colormap information is printed to stdout or saved to a plain-text file as a side effect.
     '''
-    colorMapText: str = parseColorMapDoc(args.c, args.scale, args.offset)
+    colorMapText: str = parseColorMapDoc(args.c, args.scale, args.offset, args.round)
     if args.o is None:
         print(colorMapText)
     else:
-        with open(args.o, "w") as outfp:
-            outfp.write(colorMapText)
-        print(f"Successfully wrote colormap data to {args.o}")
+        args.o.write(colorMapText)
+        print(f"Successfully wrote colormap data to {args.o.name}")
     return
 
 
@@ -203,7 +236,7 @@ def cli() -> None:
     '''
     Command line interface for this script. Invokes colorMapToTxt function as a side effect.
 
-    :return: None, invokes colorMapToTxt as a side effect.
+    :returns: None, invokes colorMapToTxt as a side effect.
     '''
     parser: argparse.ArgumentParser = argparse.ArgumentParser()
     # Normally it would be simpler to use a required positional argument, but this 
@@ -216,12 +249,14 @@ def cli() -> None:
     )
     parser.add_argument(
         "--scale",
+        type=float,
         help="Optionally specify the scale factor for the output colormap." + \
              " This can sometimes be found in the CMR variable metadata. For example," + \
              " a colormap with percent units might need --scale 0.01 to appear correct."
     )
     parser.add_argument(
         "--offset",
+        type=float,
         help="Optionally specify the offset for the output colormap." + \
              " This can sometimes be found in the CMR variable metadata. For example," + \
              " a colormap in units of degC might need --scale 273.15 to convert to Kelvin."
@@ -229,8 +264,15 @@ def cli() -> None:
     parser.add_argument(
         "-o",
         metavar="outfile",
+        type=argparse.FileType("w"), # Ascii encoding
         help="Output filename to use for the text file colormap. If unused, the colormap will" + \
              " be printed to stdout."
+    )
+    parser.add_argument(
+        "--round",
+        action="store_true",
+        help="Create the colormap with a \"round to the floor\" mode, where the same color value " + \
+             "is used across an entire quantization level, with no interpolation."
     )
     args: argparse.Namespace = parser.parse_args()
     colorMapToTxt(args)
