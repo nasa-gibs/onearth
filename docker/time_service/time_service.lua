4b9bea378130168b01c8db17e130199b800f534e
Matthew Graber <matthew.w.graber@jpl.nasa.gov>
2024-07-30 12:38:21 -0700

    Fixed time service REDIS_HOST_READER usage

--- docker/time_service/time_service.lua
+++ docker/time_service/time_service.lua
@@ -1,2 +1,2 @@
 local onearthTimeService = require "onearthTimeService"
-handler = onearthTimeService.timeService({handler_type="redis", host="{REDIS_HOST_READER}"}, {filename_format="basic"})
+handler = onearthTimeService.timeService({handler_type="redis", host="{REDIS_HOST}"}, {filename_format="basic"})
