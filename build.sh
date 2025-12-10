9f21eca3edfc4958a05d828d49f250b62e2fe5ad
grallewellyn <grace.llewellyn@jpl.nasa.gov>
2025-12-09 17:34:33 -0800

    removed test image from build.sh used for testing

--- build.sh
+++ build.sh
@@ -30,7 +30,7 @@ docker build \
     -t nasagibs/onearth-time-service:$ONEARTH_VERSION-$ONEARTH_RELEASE \
     .
 
-Build the onearth-capabilities image
+# Build the onearth-capabilities image
 docker build \
     $DOCKER_PLATFORM_OPTION \
     --no-cache \
@@ -38,16 +38,6 @@ docker build \
     -f ./docker/capabilities/Dockerfile \
     -t nasagibs/onearth-capabilities:$ONEARTH_VERSION-$ONEARTH_RELEASE \
     .
-
-# Build test image 
-docker build \
-    $DOCKER_PLATFORM_OPTION \
-    --no-cache \
-    --build-arg ONEARTH_VERSION=$ONEARTH_VERSION \
-    -f ./docker/test/Dockerfile \
-    -t nasagibs/onearth-test:$ONEARTH_VERSION-$ONEARTH_RELEASE \
-    .
-
 
 # Build the onearth-reproject image
 docker build \
